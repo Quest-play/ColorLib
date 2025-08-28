4d6957aecaae29ae17cd6701ebb13f8e27e10c91
Quest-play <82810144+Quest-play@users.noreply.github.com>
2025-08-28 22:08:10 +0300

    v1.0.0

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,8 @@
-﻿using BepInEx;
+﻿using System.Globalization;
+using BepInEx;
 using BepInEx.Logging;
-using Gatekeeper.Utility;
 using GKAPI;
+using GKAPI.Lang;
 
 namespace ColorLib;
 
@@ -9,10 +10,10 @@ namespace ColorLib;
 [BepInDependency(GKAPI.Plugin.PluginGuid)]
 public class Plugin : GkPlugin
 {
-    public const string PluginGuid = "ua.quest_play.colorlib";
+    public const string PluginGuid = "ua.quest_play.СolorLib";
     public const string PluginName = "ColorLib";
     private const string PluginVersion = "1.0.0";
-    
+
     internal new static ManualLogSource Log;
 
     public override void Load()
@@ -25,14 +26,43 @@ public class Plugin : GkPlugin
 
     public override void AddContent()
     {
-        if (EventHandler.State != EventHandler.LoadingState.PreInit)
+
+    }
+    
+    public static class ColorLib
+    {
+        public static readonly Colors LightGreen  = (Colors)0x6AFF6A;
+        public static readonly Colors Green       = (Colors)0x26C026;
+        public static readonly Colors DarkGreen   = (Colors)0x368936;
+        public static readonly Colors Blue        = (Colors)0x6A6AFF;
+        public static readonly Colors Yellow      = (Colors)0xFFFF6A;
+        public static readonly Colors Orange      = (Colors)0xFFA500;
+        public static readonly Colors Purple      = (Colors)0xA020F0;
+        public static readonly Colors White       = (Colors)0xFFFFFF;
+        public static readonly Colors Black       = (Colors)0x000000;
+
+        // Quickly create color from int hex (0xRRGGBB)
+        public static Colors FromHex(int hex) => (Colors)hex;
+
+        // Quickly create color from RGB components
+        public static Colors FromRgb(byte r, byte g, byte b)
+            => (Colors)((r << 16) | (g << 8) | b);
+
+        // Try to parse HEX strings like "#RRGGBB" or "RRGGBB"
+        public static bool TryFromHexString(string hexString, out Colors color)
         {
-            Log.LogError("Content has to be added during Pre-Init!");
-            return;
+            color = default;
+            if (string.IsNullOrWhiteSpace(hexString)) return false;
+
+            var s = hexString.Trim();
+            if (s.StartsWith("#")) s = s.Substring(1);
+            if (int.TryParse(s, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out var v))
+            {
+                color = (Colors)v;
+                return true;
+            }
+
+            return false;
         }
-
-        GlobalSettings.Instance.buildCheating = true;
-        GlobalSettings.Instance.buildDebugConsole = true;
-
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-Something
+# ColorLib
+
+A library that allows you to use HEX and RGB colors in mods. Also has built-in colors.
+
+# Dependancy
+
+- [BeepInEx - ThunderStore Link](https://thunderstore.io/c/gatekeeper/p/BepInEx/BepInExPack_IL2CPP/)
+- [GKAPI](https://thunderstore.io/c/gatekeeper/p/GatekeeperModding/GKAPI/)
+
+# Current State
+
+- Not published in Nuget
+
+# Using the Library
+
+### Build from source
+- Clone this repository with ``git clone https://github.com/Quest-play/ColorLib.git`` 
+- Navigate into the directory (if using the zip you have to unzip it first)
+- Build with the command ``dotnet build`` (requires .NET SDK 6)
+- The built dll is in the folder ``bin/Debug/net6.0/ColorLib.dll``
+
+
+# Examples of use
+
+Not yet
